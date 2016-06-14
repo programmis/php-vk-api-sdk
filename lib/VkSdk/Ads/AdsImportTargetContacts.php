1cadd7628e49d155de4bd0e8682482d04ed46c0b
Daniil Sazhin <ds@azbuka.pro>
2016-06-14 23:28:40 +0300

    ads import target contacts

---
+++ lib/VkSdk/Ads/AdsImportTargetContacts.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alfred
+ * Date: 14.06.16
+ * Time: 23:15
+ */
+
+namespace VkSdk\Ads;
+
+
+use VkSdk\Includes\Request;
+
+class AdsImportTargetContacts extends Request
+{
+    /** @var  integer */
+    private $response;
+
+    /**
+     * @return integer
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    public function setAccountId($account_id)
+    {
+        $this->vkarg_account_id = $account_id;
+        return $this;
+    }
+
+    public function setTargetGroupId($target_group_id)
+    {
+        $this->vkarg_target_group_id = $target_group_id;
+        return $this;
+    }
+
+    public function setContacts($contacts)
+    {
+        $this->vkarg_contacts = $contacts;
+        return $this;
+    }
+
+    public function doRequest()
+    {
+        $this->setRequiredParams(['account_id', 'target_group_id', 'contacts']);
+
+        $this->setMethod("ads.importTargetContacts");
+
+        $json = $this->execApi();
+        if (!$json) {
+            return false;
+        }
+
+        if (!is_object($json) && $json < 0) {
+            return $json;
+        }
+
+        if (
+            isset($json->response) && $json->response
+        ) {
+            $this->response = $json->response;
+
+            return true;
+        }
+
+        return false;
+    }
+}
