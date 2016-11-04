0e19580f6f90b4be0d9c62fc2197676c3742c4df
Daniil Sazhin <daniildr22@gmail.com>
2016-11-04 21:29:05 +0300

    user crop photo object

--- lib/VkSdk/Users/Includes/UserCropPhoto.php
+++ lib/VkSdk/Users/Includes/UserCropPhoto.php
@@ -8,6 +8,10 @@
 
 namespace VkSdk\Users\Includes;
 
+use VkSdk\Photos\Includes\Crop;
+use VkSdk\Photos\Includes\Photo;
+use VkSdk\Photos\Includes\Rect;
+
 /**
  * содержит данные о точках,
  * по которым вырезаны профильная и миниатюрная
@@ -18,5 +22,78 @@ namespace VkSdk\Users\Includes;
  */
 class UserCropPhoto
 {
+    /** @var Photo $photo */
+    private $photo;
+    /** @var Crop $crop */
+    private $crop;
+    /** @var Rect $rect */
+    private $rect;
 
+    /**
+     * объект photo фотографии пользователя,
+     * из которой вырезается главное фото профиля.
+     *
+     * @return Photo
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param Photo $photo
+     *
+     * @return UserCropPhoto
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * вырезанная фотография пользователя.
+     *
+     * @return Crop
+     */
+    public function getCrop()
+    {
+        return $this->crop;
+    }
+
+    /**
+     * @param Crop $crop
+     *
+     * @return UserCropPhoto
+     */
+    public function setCrop($crop)
+    {
+        $this->crop = $crop;
+
+        return $this;
+    }
+
+    /**
+     * миниатюрная квадратная фотография,
+     * вырезанная из фотографии crop.
+     *
+     * @return Rect
+     */
+    public function getRect()
+    {
+        return $this->rect;
+    }
+
+    /**
+     * @param Rect $rect
+     *
+     * @return UserCropPhoto
+     */
+    public function setRect($rect)
+    {
+        $this->rect = $rect;
+
+        return $this;
+    }
 }
