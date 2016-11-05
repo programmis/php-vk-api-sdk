b863771789815e083ad736fd6cbd0b1d011c2b64
Daniil Sazhin <daniildr22@gmail.com>
2016-11-05 21:32:26 +0300

    groups join

--- lib/VkSdk/Groups/GroupsJoin.php
+++ lib/VkSdk/Groups/GroupsJoin.php
@@ -4,38 +4,73 @@ namespace VkSdk\Groups;
 
 use VkSdk\Includes\Request;
 
+/**
+ * Данный метод позволяет вступить в группу, публичную страницу,
+ * а также подтвердить участие во встрече.
+ *
+ * Class GroupsJoin
+ * @package VkSdk\Groups
+ */
 class GroupsJoin extends Request
 {
+    /**
+     * идентификатор сообщества.
+     *
+     * @param int $group_id
+     *
+     * @return $this
+     */
     public function setGroupId($group_id)
     {
         $this->vkarg_group_id = $group_id;
+
         return $this;
     }
 
+    /**
+     * опциональный параметр, учитываемый, если group_id принадлежит встрече.
+     * true — Возможно пойду. false — Точно пойду.
+     * По умолчанию false.
+     *
+     * @param $not_sure
+     *
+     * @return $this
+     */
     public function setNotSure($not_sure)
     {
-        $this->vkarg_not_sure = $not_sure;
+        $this->vkarg_not_sure = $not_sure ? '1' : '0';
+
         return $this;
     }
 
+    /** @inheritdoc */
+    public function getMethod()
+    {
+        return "groups.join";
+    }
 
+    /** @inheritdoc */
+    public function getApiVersion()
+    {
+        return '5.60';
+    }
+
+    /**
+     * В случае успешного вступления метод вернёт true
+     *
+     * {@inheritdoc}
+     */
     public function doRequest()
     {
         $this->setRequiredParams('group_id');
 
-        $this->setMethod("groups.join");
 
-        $json = $this->execApi();
-        if (!$json) {
-            return false;
-        }
+        $result = $this->execApi();
 
-        if (!is_object($json) && $json < 0) {
-            return $json;
-        }
-
-        if (isset($json->response)) {
-            return true;
+        if ($result && ($json = $this->getJsonResponse())) {
+            if (isset($json->response)) {
+                return true;
+            }
         }
 
         return false;
