39ce8439afd41972b1290316107bc245251736b9
Daniil Sazhin <daniildr22@gmail.com>
2016-11-10 21:02:44 +0300

    AccountSetInfo result

--- lib/VkSdk/Account/AccountSetInfo.php
+++ lib/VkSdk/Account/AccountSetInfo.php
@@ -6,12 +6,12 @@ use VkSdk\Includes\Request;
 
 /**
  * Allows to edit the current account info.
+ *
  * Class AccountSetInfo
  * @package VkSdk\Account
  */
 class AccountSetInfo extends Request
 {
-
     /**
      * See constants of class OkResponse
      *
@@ -20,6 +20,8 @@ class AccountSetInfo extends Request
     private $response;
 
     /**
+     * result in $this->getResponse();
+     *
      * {@inheritdoc}
      */
     public function doRequest()
@@ -27,6 +29,8 @@ class AccountSetInfo extends Request
         $result = $this->execApi();
         if ($result && ($json = $this->getJsonResponse())) {
             if (isset($json->response) && $json->response) {
+                $this->response = $json->response;
+
                 return true;
             }
         }
