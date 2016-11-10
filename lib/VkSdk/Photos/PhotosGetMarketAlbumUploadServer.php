76f6e8f290c19c3e2f2377a14a3c0e14e2bbbd67
Daniil Sazhin <sazhin_daniil@mail.ru>
2016-11-10 16:00:58 +0300

    upload in PhotosGetMarketAlbumUploadServer

--- lib/VkSdk/Photos/PhotosGetMarketAlbumUploadServer.php
+++ lib/VkSdk/Photos/PhotosGetMarketAlbumUploadServer.php
@@ -3,11 +3,13 @@
 namespace VkSdk\Photos;
 
 use VkSdk\Includes\Request;
+use VkSdk\Photos\Includes\UploadResult;
 
 /**
  * Returns the server address for market album photo upload.
  * Class PhotosGetMarketAlbumUploadServer
- * @package VkSdk\Photos
+ *
+*@package VkSdk\Photos
  */
 class PhotosGetMarketAlbumUploadServer extends Request
 {
@@ -18,6 +20,48 @@ class PhotosGetMarketAlbumUploadServer extends Request
     private $upload_url;
 
     /**
+     * @var UploadResult $uploadResult
+     */
+    private $uploadResult;
+
+    /**
+     * @return UploadResult
+     */
+    public function getUploadResult()
+    {
+        return $this->uploadResult;
+    }
+
+    /**
+     * @param array $files
+     *
+     * @return bool
+     */
+    public function upload($files)
+    {
+        $result = $this->uploadFiles($this->getUploadUrl(), $files);
+        if ($result && ($json = $this->getJsonResponse())) {
+            $this->uploadResult = new UploadResult();
+            $this->uploadResult->fillByJson($json);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * URL to upload the photo
+     *
+     * @return string
+     */
+    public function getUploadUrl()
+    {
+        return $this->upload_url;
+    }
+
+    /**
+     * result in $this->getUploadUrl();
      * {@inheritdoc}
      */
     public function doRequest()
@@ -27,6 +71,8 @@ class PhotosGetMarketAlbumUploadServer extends Request
         $result = $this->execApi();
         if ($result && ($json = $this->getJsonResponse())) {
             if (isset($json->response) && $json->response) {
+                $this->upload_url = $json->response->upload_url;
+
                 return true;
             }
         }
@@ -48,16 +94,6 @@ class PhotosGetMarketAlbumUploadServer extends Request
     public function getMethod()
     {
         return "photos.getMarketAlbumUploadServer";
-    }
-
-    /**
-     * URL to upload the photo
-     *
-     * @return string
-     */
-    public function getUploadUrl()
-    {
-        return $this->upload_url;
     }
 
     /**
