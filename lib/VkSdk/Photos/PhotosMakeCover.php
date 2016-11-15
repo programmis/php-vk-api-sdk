d8ac7745003d18b71cbdb71f4115334fbc36725a
Daniil Sazhin <daniildr22@gmail.com>
2016-11-15 21:44:42 +0300

    PhotosMakeCover response

--- lib/VkSdk/Photos/PhotosMakeCover.php
+++ lib/VkSdk/Photos/PhotosMakeCover.php
@@ -6,12 +6,12 @@ use VkSdk\Includes\Request;
 
 /**
  * Makes a photo into an album cover.
+ *
  * Class PhotosMakeCover
  * @package VkSdk\Photos
  */
 class PhotosMakeCover extends Request
 {
-
     /**
      * See constants of class OkResponse
      *
@@ -20,6 +20,8 @@ class PhotosMakeCover extends Request
     private $response;
 
     /**
+     * result in $this->getResponse();
+     *
      * {@inheritdoc}
      */
     public function doRequest()
@@ -29,6 +31,8 @@ class PhotosMakeCover extends Request
         $result = $this->execApi();
         if ($result && ($json = $this->getJsonResponse())) {
             if (isset($json->response) && $json->response) {
+                $this->response = $json->response;
+
                 return true;
             }
         }
