79031d6e3cd1e9282991d57bf238f2bc883d45e2
Daniil Sazhin <daniildr22@gmail.com>
2016-11-15 21:53:24 +0300

    VideoAdd response

--- lib/VkSdk/Video/VideoAdd.php
+++ lib/VkSdk/Video/VideoAdd.php
@@ -6,12 +6,12 @@ use VkSdk\Includes\Request;
 
 /**
  * Adds a video to a user or community page.
+ *
  * Class VideoAdd
  * @package VkSdk\Video
  */
 class VideoAdd extends Request
 {
-
     /**
      * See constants of class OkResponse
      *
@@ -20,6 +20,8 @@ class VideoAdd extends Request
     private $response;
 
     /**
+     * result in $this->getResponse();
+     *
      * {@inheritdoc}
      */
     public function doRequest()
@@ -29,6 +31,8 @@ class VideoAdd extends Request
         $result = $this->execApi();
         if ($result && ($json = $this->getJsonResponse())) {
             if (isset($json->response) && $json->response) {
+                $this->response = $json->response;
+
                 return true;
             }
         }
