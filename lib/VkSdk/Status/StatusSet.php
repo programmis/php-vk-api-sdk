6e209593f73522c99847d8595b1c7f4a8ce996d4
Daniil Sazhin <daniildr22@gmail.com>
2016-11-15 21:51:51 +0300

    StatusSet response

--- lib/VkSdk/Status/StatusSet.php
+++ lib/VkSdk/Status/StatusSet.php
@@ -6,12 +6,12 @@ use VkSdk\Includes\Request;
 
 /**
  * Sets a new status for the current user.
+ *
  * Class StatusSet
  * @package VkSdk\Status
  */
 class StatusSet extends Request
 {
-
     /**
      * See constants of class OkResponse
      *
@@ -20,6 +20,8 @@ class StatusSet extends Request
     private $response;
 
     /**
+     * result in $this->getResponse();
+     *
      * {@inheritdoc}
      */
     public function doRequest()
@@ -27,6 +29,8 @@ class StatusSet extends Request
         $result = $this->execApi();
         if ($result && ($json = $this->getJsonResponse())) {
             if (isset($json->response) && $json->response) {
+                $this->response = $json->response;
+
                 return true;
             }
         }
